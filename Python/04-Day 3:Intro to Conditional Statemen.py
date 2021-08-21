c7d9f0e5529c743f7c04fd60f6515d5b7a51fc24
Sanket Sambhaji Patil <73840483+SanketPatil5248@users.noreply.github.com>
2021-08-21 07:50:05 +0530

    Update and rename 04-Day 3 to 04-Day 3:Intro to Conditional Statemen.py

---
+++ Python/04-Day 3:Intro to Conditional Statemen.py
@@ -0,0 +1,32 @@
+# ========================
+#       Information
+# ========================
+
+# problem link : https://www.hackerrank.com/challenges/30-hello-world/problem
+# Language: Python
+
+# ========================
+#         Solution
+# ========================
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+if N%2!=0:
+    print("Weird")
+else:
+    if N in range(2,5):
+        print("Not Weird")
+    elif N in range(6,21):
+        print("Weird")
+    else:
+        print("Not Weird")    
